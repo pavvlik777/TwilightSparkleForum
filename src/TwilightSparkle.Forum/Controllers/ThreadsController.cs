7158fe7c01e130359334f6ebc0b6b787155e76c1
pavvlik777 <41309600+pavvlik777@users.noreply.github.com>
2020-05-31 16:27:09 +0300

    added markdown editor

---
+++ src/TwilightSparkle.Forum/Controllers/ThreadsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TwilightSparkle.Forum.Controllers
+{
+    [Authorize]
+    public class ThreadsController : Controller
+    {
+        public IActionResult CreateThread()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateThread(int i) //TODO
+        {
+            return View();
+        }
+    }
+}
